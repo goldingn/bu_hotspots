214c62440f502fe0de92d609087c91d5b3c2235f
Nick Golding <goldingn35@gmail.com>
2022-01-26 16:28:35 +0800

    add CV code

---
+++ R/split_data.R
@@ -0,0 +1,45 @@
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#'
+#' @title
+#' @param meshblock_incidence_survey_periods_blocked
+#' @param which
+#' @return
+#' @author Nick Golding
+#' @export
+split_data <- function(blocked, which = c("train", "test")) {
+  
+  which <- match.arg(which)
+  
+  blocks <- unique(blocked$block)
+  
+  splits <- list()
+  
+  # split into three different training sets, and corresponding test sets
+  for (this_block in blocks) {
+    
+    # this_block is used for testing, but not training
+    if (which == "train") {
+      
+      splits[[this_block]] <- meshblock_incidence_survey_periods_blocked %>%
+        filter(
+          block != this_block
+        )
+      
+    } else {
+      
+      splits[[this_block]] <- meshblock_incidence_survey_periods_blocked %>%
+        filter(
+          block == this_block
+        )
+      
+    }
+    
+  }
+  
+  names(splits) <- blocks
+  
+  splits
+  
+}
